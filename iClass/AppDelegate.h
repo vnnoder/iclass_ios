76fc254ba5c03b0378845bd2ced08bf12a185196
Wiely Rabin <wiely@Wielys-MacBook-Pro.local>
2013-04-27 16:52:54 +0800

    just for testing
    wiely test

--- iClass/AppDelegate.h
+++ iClass/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by Ma Xiangmei on 27/4/13.
 //  Copyright (c) 2013 Lewis He Zhihua. All rights reserved.
 //
-
+// just testing 
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
