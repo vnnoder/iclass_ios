5038b2efa74d81e862d417a603dee11a76e890eb
kingkenmark <kingkenmark@gmail.com>
2013-04-28 11:36:39 +0800

    Create initial project

---
+++ iClass/iClass/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  iClass
+//
+//  Created by MagicStudio on 13-4-28.
+//  Copyright (c) 2013年 ISS. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iClass/iClass/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iClass
+//
+//  Created by MagicStudio on 13-4-28.
+//  Copyright (c) 2013年 ISS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
