fe59d44a50f29a12a12b2cf8d57a26d4d72d9a44
LewisHe <hzhwine@gmail.com>
2013-05-05 22:54:45 +0800

    clean code

--- Project/iclass/main.m
+++ Project/iclass/main.m
@@ -16,29 +16,6 @@
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-//        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-        
-        
-        // get question
-       NSData* response =  [HttpQuery querySyncWithPath:@"/talks.json" withMethod:@"GET" withParams:nil];
-//        NSLog(@"=====> %@", response);
-//
-       NSMutableDictionary *params = [NSMutableDictionary dictionary];
-        [params setObject:@"description" forKey:@"talk[description]"];
-        [params setObject:@"title" forKey:@"talk[title]"];
-        [params setObject:@"1" forKey:@"talk[user_id]"];
-        
-     //
-        
-        response =  [HttpQuery querySyncWithPath:@"/talks.json" withMethod:@"POST" withParams:params];
-        NSString *json = [[NSString alloc] initWithData:response encoding:NSUTF8StringEncoding];
-
-        NSLog(@"=====> %@", json);
-       Session* sess =  [SessionSerializer deserialize:response];
-        NSLog(@"id = %d", [sess key]);
-        
-//        NSError *error = [[NSError alloc]init];
-        
-//        [QueryUtil request:@"/talks.json" method:@"POST" params:params host:@"host" withError:error];
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
