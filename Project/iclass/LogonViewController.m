dfb673668bfa182d692a2f0ecab0068dd1d001c8
Wiely Rabin <wielyrabin@gmail.com>
2013-05-12 20:24:38 +0800

    secure text

--- Project/iclass/LogonViewController.m
+++ Project/iclass/LogonViewController.m
@@ -22,6 +22,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+
     }
     return self;
 }
@@ -30,7 +31,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    
+    self.passwordField.secureTextEntry = YES;    
 }
 
 - (void)didReceiveMemoryWarning
